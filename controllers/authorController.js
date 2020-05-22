825aa37209030b5fb4a823a8731e194c3dba8adb
Joseph Horman <JosephLHorman@gmail.com>
2020-05-22 14:57:21 -0600

    Minor bugfix

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -84,14 +84,14 @@ module.exports = {
     author_delete_get: (req, res, next) => {
         async.parallel({
             author: (callback) => Author.findById(req.params.id).exec(callback),
-            authors_books: Book.find({'author': req.params.id }).exec(callback)
+            authors_books: (callback) => Book.find({'author': req.params.id }).exec(callback)
         }, (err, results) => {
             if(err) {return next(err);}
             if(results.author == null) {
                 res.redirect('/catalog/authors');
             }
 
-            res.render('author_delete', {title: "Delete Author", author: results.author, author_books: results.authorsbooks});
+            res.render('author_delete', {title: "Delete Author", author: results.author, author_books: results.authors_books});
         });
     },
     author_delete_post: (req, res, next) => {
@@ -103,7 +103,7 @@ module.exports = {
 
             if(results.authors_books.length > 0) {
                 //Author has books, render same as get method
-                res.render('author_delete', {title: "Delete Author", author: results.author, author_books: results.authorsbooks});
+                res.render('author_delete', {title: "Delete Author", author: results.author, author_books: results.authors_books});
                 return;
             } else {
                 //no books, remove author and return to author list
